16b97cf23109a54ed3affe71af6f9897590beb07
norayr <norayr@arnet.am>
2017-07-28 16:36:32 +0400

    fix

--- lib/links.sh
+++ lib/links.sh
@@ -7,13 +7,12 @@ source ./out.sh
 # first param is where to link
 # second param is a broken link to fix   
 
-function makeLink
+function fixLink
 {
-
    if [ -L $2 ] ; then
      if [ -e $2 ] ; then
         OutRedLine "ACHTUNG "
-        OutLine "${tmplnk} is good link, did you remove the link destination?"
+        OutLine "${tmplnk} is a good link, did you remove the link destination?"
      else
         OutGreenLine "  OK "
         OutLine "$2 is a broken link, as expected. fixing: "
@@ -29,7 +28,6 @@ function makeLink
       OutLine "$2 is missing. check yourself."
    fi
    OutLn
-
 }
 
 
